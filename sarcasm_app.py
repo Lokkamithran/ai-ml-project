7bf0b7afc1103abf6d92c067a6e964b5d937a4c9
Hedhav <83498137+Hedhav@users.noreply.github.com>
2022-11-20 15:09:51 +0530

    [FEAT]: Web integration (#1)

--- sarcasm_app.py
+++ sarcasm_app.py
@@ -1,13 +1,30 @@
+from flask import Flask, render_template, request
 import pickle
+
+app = Flask(__name__)
+# model = pickle.load(open('model.pkl', 'rb'))
 
 loaded_cv = pickle.load(open('sarcasm_vectorizer.pkl', 'rb'))
 loaded_model = pickle.load(open('sarcasm_model.pkl', 'rb'))
 
-user = input("Enter text: ")
-data = loaded_cv.transform([user]).toarray()
-output = loaded_model.predict(data)
 
-if output[0] == 'Sarcasm':
-    print('It\'s a sarcastic headline.')
-else:
-    print('It\'s not a sarcastic headline.')
+@app.route("/")
+def hello():
+    return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+
+    sentence = request.form['sentence']
+    data = loaded_cv.transform([sentence]).toarray()
+    output = loaded_model.predict(data)
+
+    printtext = ""
+    if output[0] == 'Sarcasm':
+        printtext = "It\'s a sarcastic headline."
+    else:
+        printtext = "It\'s not a sarcastic headline."
+    return render_template('index.html', prediction_text=printtext)
+
+if __name__ == "__main__":
+    app.run()
